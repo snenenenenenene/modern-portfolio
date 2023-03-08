6fb35abff80c26a9b10e1ea9271628d55766cf8f
snenenenenenene <sennebels@gmail.com>
2023-03-08 20:21:33 +0100

    feat: :art: added 3d model

---
+++ app/components/common/Model.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Model(props: any) {
+  const { nodes, materials }: any = useGLTF("/assets/pangolin.glb");
+  return (
+    <group {...props} dispose={null}>
+      <group position={[0, 19.75, 0]} scale={6.68}>
+        <mesh
+          geometry={nodes.Cube001.geometry}
+          material={materials["Material.002"]}
+        />
+        <mesh
+          geometry={nodes.Cube001_1.geometry}
+          material={materials["Material.003"]}
+        />
+        <mesh
+          geometry={nodes.Cube001_2.geometry}
+          material={materials["Material.004"]}
+        />
+        <mesh
+          geometry={nodes.Cube001_3.geometry}
+          material={materials["Material.005"]}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/pangolin.glb");
