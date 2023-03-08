4ec4c86c0dbc0951389df0406503abdcf056b729
snenenenenenene <sennebels@gmail.com>
2023-03-08 20:22:17 +0100

    feat: :sparkles: made navbar collapsing

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
 
 export const Navbar = () => {
+  const navbar = useRef<any>(null);
+  const [show, setShow] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  function controlNavbar() {
+    if (typeof window !== "undefined") {
+      if (window.scrollY < lastScrollY || window.scrollY < 32) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <nav className=" font-body w-full h-14 border-b border-light-secondary flex">
+    <nav
+      ref={navbar}
+      className={`${
+        show ? "h-14 border-b border-light-secondary" : "h-0"
+      } fixed z-50 bg-light-primary overflow-hidden font-body w-full  flex`}
+    >
       <section className="border-r text-5xl border-light-secondary flex justify-center items-center w-40 h-full">
         <h2 className="font-display tracking-wide">Senne Bels</h2>
       </section>
