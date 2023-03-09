3ee94bfdf5d98c562dd082d5c981bed5f02f3d58
snenenenenenene <sennebels@gmail.com>
2023-03-09 15:07:05 +0100

    feat: :sparkles: updated head

---
+++ app/defaultTags.tsx
@@ -0,0 +1,8 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="/assets/images/sun.png" rel="shortcut icon" />
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,33 @@
+import Head from "next/head";
 import { ReactNode } from "react";
 import { Navbar } from "./components/common/Navbar";
 import "./globals.css";
 
 export const metadata = {
-  title: "Senne Bels",
-  description: "Your local frontend developer",
-  icons: {
-    icon: "/public/assets/lynx.png",
+  openGraph: {
+    title: "Senne Bels",
+    description: "Your favourite frontend developer",
+    url: "https://www.sennebels.xyz",
+    images: [
+      {
+        url: "https://sennebels.xyz/assets/images/twitter-pic.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+  robots: {
+    index: true,
   },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      {/* <Head /> */}
+      <Head>
+        <link rel="shortcut icon" href="assets/sun.ico" />
+      </Head>
       <body>
         <div className="w-screen scroll-smooth overflow-x-hidden text-light-secondary dark:text-dark-secondary flex flex-col bg-light-primary dark:bg-dark-primary">
           <Navbar />
