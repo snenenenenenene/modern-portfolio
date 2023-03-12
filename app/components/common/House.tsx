af3145dd98142c177f03aa7f26ffbc752c55761d
snenenenenenene <sennebels@gmail.com>
2023-03-12 16:21:25 +0100

    feat: :sparkles: added the house component

---
+++ app/components/common/House.tsx
@@ -0,0 +1,75 @@
+import { useGLTF } from "@react-three/drei";
+import { useRef } from "react";
+
+export function House(props: any) {
+  const group = useRef();
+  const { nodes, materials }: any = useGLTF("/assets/house.gltf");
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group name="House" position={[0, 1.02, 2.59]}>
+          <mesh
+            name="Cube001"
+            geometry={nodes.Cube001.geometry}
+            material={materials.Roof}
+          />
+          <mesh
+            name="Cube001_1"
+            geometry={nodes.Cube001_1.geometry}
+            material={materials.Wall}
+          />
+          <mesh
+            name="Cube001_2"
+            geometry={nodes.Cube001_2.geometry}
+            material={materials.Wood}
+          />
+          <mesh
+            name="Cube001_3"
+            geometry={nodes.Cube001_3.geometry}
+            material={materials["Patio floor"]}
+          />
+          <mesh
+            name="Cube001_4"
+            geometry={nodes.Cube001_4.geometry}
+            material={materials.Windows}
+          />
+          <mesh
+            name="Cube001_5"
+            geometry={nodes.Cube001_5.geometry}
+            material={materials.Door}
+          />
+          <mesh
+            name="Cube001_6"
+            geometry={nodes.Cube001_6.geometry}
+            material={materials["Room floor"]}
+          />
+          <mesh
+            name="Cube001_7"
+            geometry={nodes.Cube001_7.geometry}
+            material={materials["Room wall"]}
+          />
+          <mesh
+            name="Cube001_8"
+            geometry={nodes.Cube001_8.geometry}
+            material={materials["Room celing"]}
+          />
+          <mesh
+            name="Cube001_9"
+            geometry={nodes.Cube001_9.geometry}
+            material={materials.Planks}
+          />
+        </group>
+        <mesh
+          name="Ladder"
+          geometry={nodes.Ladder.geometry}
+          material={nodes.Ladder.material}
+          position={[0.65, 0, -2.54]}
+          rotation={[-0.03, -0.07, 0.23]}
+          scale={0.03}
+        />
+      </group>
+    </group>
+  );
+}
+
+useGLTF.preload("/house.gltf");
