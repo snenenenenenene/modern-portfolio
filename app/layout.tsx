70b846c7a89ea3f057cf0c24ec9011a024f92939
snenenenenenene <sennebels@gmail.com>
2023-03-12 09:52:57 +0100

    feat: :sparkles: twitter card

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       {/* <Head /> */}
       <Head>
         <link rel="shortcut icon" href="assets/sun.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:title" content="sennebels.xyz" />
+        <meta
+          name="twitter:description"
+          content="Your favourite frontend developer"
+        />
+        <meta
+          name="twitter:image"
+          content="https://sennebels.xyz/assets/images/twitter-pic.png"
+        />
       </Head>
       <body>
         <div className="w-screen scroll-smooth overflow-x-hidden text-light-secondary dark:text-dark-secondary flex flex-col bg-light-primary dark:bg-dark-primary">
